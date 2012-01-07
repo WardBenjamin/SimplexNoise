35b2c28e44f8b79e290c42d1079b2f1e127184dc
heikki.to4@gmail.com <heikki.to4@gmail.com@8b035035-613e-1210-6c01-e097875929ff>
2012-01-07 20:22:52 +0000

    First import.

---
+++ trunk/NoiseTest/NoiseExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NoiseTest
+{
+    static class NoiseExample
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}

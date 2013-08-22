8483fe8edab2c7dd4072e4f41e9795ee01e7b785
heikki.to4@gmail.com <heikki.to4@gmail.com@8b035035-613e-1210-6c01-e097875929ff>
2013-08-22 15:45:50 +0000

    NoiseTest improvements by Draco Rat.

--- NoiseTest/Form1.Designer.cs
+++ NoiseTest/Form1.Designer.cs
@@ -28,20 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ControlPanel = new System.Windows.Forms.Panel();
+            this.OverrideSeed = new System.Windows.Forms.CheckBox();
+            this.NewSeedLabel = new System.Windows.Forms.Label();
+            this.NewSeed = new System.Windows.Forms.TextBox();
+            this.GridSizeLabel = new System.Windows.Forms.Label();
+            this.GridSize = new System.Windows.Forms.HScrollBar();
+            this.GridMaximumLabel = new System.Windows.Forms.Label();
+            this.GridMinimumLabel = new System.Windows.Forms.Label();
+            this.TimeTakenLabel = new System.Windows.Forms.Label();
+            this.ControlPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ControlPanel
+            // 
+            this.ControlPanel.Controls.Add(this.TimeTakenLabel);
+            this.ControlPanel.Controls.Add(this.GridMinimumLabel);
+            this.ControlPanel.Controls.Add(this.GridMaximumLabel);
+            this.ControlPanel.Controls.Add(this.GridSize);
+            this.ControlPanel.Controls.Add(this.GridSizeLabel);
+            this.ControlPanel.Controls.Add(this.NewSeed);
+            this.ControlPanel.Controls.Add(this.NewSeedLabel);
+            this.ControlPanel.Controls.Add(this.OverrideSeed);
+            this.ControlPanel.Location = new System.Drawing.Point(12, 12);
+            this.ControlPanel.Name = "ControlPanel";
+            this.ControlPanel.Size = new System.Drawing.Size(538, 122);
+            this.ControlPanel.TabIndex = 0;
+            // 
+            // OverrideSeed
+            // 
+            this.OverrideSeed.AutoSize = true;
+            this.OverrideSeed.Location = new System.Drawing.Point(4, 4);
+            this.OverrideSeed.Name = "OverrideSeed";
+            this.OverrideSeed.Size = new System.Drawing.Size(130, 21);
+            this.OverrideSeed.TabIndex = 0;
+            this.OverrideSeed.Text = "Override Seed?";
+            this.OverrideSeed.UseVisualStyleBackColor = true;
+            this.OverrideSeed.CheckStateChanged += new System.EventHandler(this.RecalculateNoise);
+            // 
+            // NewSeedLabel
+            // 
+            this.NewSeedLabel.AutoSize = true;
+            this.NewSeedLabel.Location = new System.Drawing.Point(35, 32);
+            this.NewSeedLabel.Name = "NewSeedLabel";
+            this.NewSeedLabel.Size = new System.Drawing.Size(80, 17);
+            this.NewSeedLabel.TabIndex = 1;
+            this.NewSeedLabel.Text = "New Seed?";
+            // 
+            // NewSeed
+            // 
+            this.NewSeed.Enabled = false;
+            this.NewSeed.Location = new System.Drawing.Point(137, 29);
+            this.NewSeed.Name = "NewSeed";
+            this.NewSeed.Size = new System.Drawing.Size(100, 22);
+            this.NewSeed.TabIndex = 2;
+            this.NewSeed.Text = "12345";
+            this.NewSeed.TextChanged += new System.EventHandler(this.RecalculateNoise);
+            // 
+            // GridSizeLabel
+            // 
+            this.GridSizeLabel.AutoSize = true;
+            this.GridSizeLabel.Location = new System.Drawing.Point(4, 66);
+            this.GridSizeLabel.Name = "GridSizeLabel";
+            this.GridSizeLabel.Size = new System.Drawing.Size(92, 17);
+            this.GridSizeLabel.TabIndex = 3;
+            this.GridSizeLabel.Text = "Grid Size (1):";
+            // 
+            // GridSize
+            // 
+            this.GridSize.Location = new System.Drawing.Point(137, 66);
+            this.GridSize.Minimum = 1;
+            this.GridSize.Name = "GridSize";
+            this.GridSize.Size = new System.Drawing.Size(320, 21);
+            this.GridSize.TabIndex = 4;
+            this.GridSize.Value = 1;
+            this.GridSize.Scroll += new System.Windows.Forms.ScrollEventHandler(this.RecalculateNoise);
+            // 
+            // GridMaximumLabel
+            // 
+            this.GridMaximumLabel.AutoSize = true;
+            this.GridMaximumLabel.Location = new System.Drawing.Point(472, 66);
+            this.GridMaximumLabel.Name = "GridMaximumLabel";
+            this.GridMaximumLabel.Size = new System.Drawing.Size(32, 17);
+            this.GridMaximumLabel.TabIndex = 5;
+            this.GridMaximumLabel.Text = "100";
+            // 
+            // GridMinimumLabel
+            // 
+            this.GridMinimumLabel.AutoSize = true;
+            this.GridMinimumLabel.Location = new System.Drawing.Point(109, 66);
+            this.GridMinimumLabel.Name = "GridMinimumLabel";
+            this.GridMinimumLabel.Size = new System.Drawing.Size(16, 17);
+            this.GridMinimumLabel.TabIndex = 6;
+            this.GridMinimumLabel.Text = "1";
+            // 
+            // TimeTakenLabel
+            // 
+            this.TimeTakenLabel.AutoSize = true;
+            this.TimeTakenLabel.Location = new System.Drawing.Point(7, 102);
+            this.TimeTakenLabel.Name = "TimeTakenLabel";
+            this.TimeTakenLabel.Size = new System.Drawing.Size(0, 17);
+            this.TimeTakenLabel.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 515);
+            this.ClientSize = new System.Drawing.Size(679, 634);
+            this.Controls.Add(this.ControlPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Noise Example";
+            this.Resize += new System.EventHandler(this.RecalculateNoise);
+            this.ControlPanel.ResumeLayout(false);
+            this.ControlPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel ControlPanel;
+        private System.Windows.Forms.Label GridMinimumLabel;
+        private System.Windows.Forms.Label GridMaximumLabel;
+        private System.Windows.Forms.HScrollBar GridSize;
+        private System.Windows.Forms.Label GridSizeLabel;
+        private System.Windows.Forms.TextBox NewSeed;
+        private System.Windows.Forms.Label NewSeedLabel;
+        private System.Windows.Forms.CheckBox OverrideSeed;
+        private System.Windows.Forms.Label TimeTakenLabel;
 
     }
 }
--- NoiseTest/Form1.cs
+++ NoiseTest/Form1.cs
@@ -12,39 +12,115 @@ namespace NoiseTest
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-
-            CreateBitmapAtRuntime();
-        }
-
+        private byte[] byteBackup;
         PictureBox pictureBox1 = new PictureBox();
 
-        public void CreateBitmapAtRuntime()
+        public Form1()
         {
-            pictureBox1.Size = new Size(510, 510);
-            this.Controls.Add(pictureBox1);
+            byteBackup = new byte[Noise.perm.Length];
+            Noise.perm.CopyTo(byteBackup, 0);
 
-            Bitmap flag = new Bitmap(500, 500);
+            InitializeComponent();
+
+            this.GridSize.Value = 3;
+            this.RecalculateNoise(null, null);
+
+            this.Controls.Add(this.pictureBox1);
+        }
+
+        private int GetInt(TextBox textBox)
+        {
+            int i = 0;
+            return int.TryParse(textBox.Text, out i) ? i : 0;
+        }
+
+        public void CreateBitmapAtRuntime(int gridSize)
+        {
+            int padding = this.ControlPanel.Top;
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            this.pictureBox1.Top = this.ControlPanel.Bottom + padding;
+            this.pictureBox1.Left = this.ControlPanel.Left;
+            // this.Controls.Add(pictureBox1);
+
+            int width = this.ClientSize.Width - padding * 2;
+            int height = this.ClientSize.Height - this.ControlPanel.Bottom - padding * 2;
+
+            pictureBox1.Size = new Size(width, height);
+
+            stopwatch.Start();
+
+            Bitmap flag = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            Rectangle flagRect = new Rectangle(0, 0, width, height);
             Graphics flagGraphics = Graphics.FromImage(flag);
 
-            for (int x = 0; x < 500; ++x)
-            {
-                for (int y = 0 ; y < 500 ; ++y)
-                {
-                    int cval = (int)(Noise.Generate(x / 200f, y / 200f) * 128 + 128);
-                    
-                    Color col = Color.FromArgb(cval, cval, cval);
-                    SolidBrush brush = new SolidBrush(col);
+            System.Drawing.Imaging.BitmapData flagData = flag.LockBits(flagRect, System.Drawing.Imaging.ImageLockMode.ReadWrite, flag.PixelFormat);
+            int imageByteSize = flagData.Stride * flagData.Height;
+            byte[] destinationData = new byte[imageByteSize];
+            int bitsPerPixelElement = 32 / 8;
 
-                    Rectangle rect = new Rectangle(x, y, 1, 1);
-                    flagGraphics.FillRectangle(brush, rect);
+            byte[] noiseSeed;
+            if (this.OverrideSeed.Checked && (GetInt(this.NewSeed) > 0))
+            {
+                noiseSeed = new byte[512];
+                Random rand = new Random(GetInt(this.NewSeed));
+                rand.NextBytes(noiseSeed);
+            }
+            else
+            {
+                noiseSeed = this.byteBackup;
+            }
+
+            Noise.perm = noiseSeed;
+
+            for (int x = 0; x < width; x+= gridSize)
+            {
+                for (int y = 0 ; y < height ; y+= gridSize)
+                {
+                    byte cval = (byte)(Noise.Generate(x / 100f, y / 100f) * 128 + 128);
+
+                    for (int i = 0; i < gridSize; i++)
+                    {
+                        if ((x + i) >= width) break;
+                        for (int j = 0; j < gridSize; j++)
+                        {
+                            if ((y + j) >= height) break;
+                            destinationData[(y + j) * flagData.Stride + (x + i) * bitsPerPixelElement + 2] = (byte)((float)cval * 0.6); // R
+                            destinationData[(y + j) * flagData.Stride + (x + i) * bitsPerPixelElement + 1] = (byte)((float)cval * 0.6); // G
+                            destinationData[(y + j) * flagData.Stride + (x + i) * bitsPerPixelElement] = cval; // B
+                        }
+                    }
+                    
+                    //Color col = Color.FromArgb((int)(cval*0.6f), (int)(cval*0.6f), cval);
+                    //SolidBrush brush = new SolidBrush(col);
+
+                    //Rectangle rect = new Rectangle(x, y, gridSize, gridSize);
+                    //flagGraphics.FillRectangle(brush, rect);
                 }
             }
-            
-            pictureBox1.Image = flag;
 
+            System.Runtime.InteropServices.Marshal.Copy(destinationData, 0, flagData.Scan0, destinationData.Length);
+            flag.UnlockBits(flagData);
+
+            stopwatch.Stop();
+
+            pictureBox1.Image = flag;
+            pictureBox1.Invalidate();
+
+            this.TimeTakenLabel.Text = string.Format("Time taken to generate noise: {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        private void RecalculateNoise(object sender, EventArgs e)
+        {
+            this.NewSeed.Enabled = this.OverrideSeed.Checked;
+            int gridSize = this.GridSize.Value;
+            CreateBitmapAtRuntime(gridSize);
+        }
+
+        private void RecalculateNoise(object sender, ScrollEventArgs e)
+        {
+            this.GridSizeLabel.Text = string.Format("Grid Size ({0}):", this.GridSize.Value);
+            int gridSize = this.GridSize.Value;
+            CreateBitmapAtRuntime(gridSize);
         }
     }
 }
